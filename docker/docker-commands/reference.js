db775ee415eedc09d38a938a9600e64d53cc9d36
raffael marinho <silvamarinho10@gmail.com>
2022-02-22 22:18:09 -0300

    feat:Generate a build from the backend Dockerfile

---
+++ docker/docker-commands/reference.js
@@ -0,0 +1,7 @@
+// https://elias.praciano.com/2016/02/como-baixar-e-usar-imagens-prontas-do-repositorio-docker/
+// https://docs.docker.com/engine/reference/commandline/exec/
+// https://stack.desenvolvedor.expert/appendix/docker/comandos.html
+// https://jtemporal.com/brincando-com-a-listagem-de-containers-docker/
+// https://docs.docker.com/engine/reference/commandline/ps/
+
+//contei com ajuda da mentoria
